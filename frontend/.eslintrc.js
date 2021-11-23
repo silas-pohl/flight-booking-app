6fae242939b831f82b6fe118ac718c5938a8e6f6
silas-pohl <mail@silaspohl.de>
2021-11-23 12:38:45 +0100

    Added missing testing libraries and Carbon Design

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+    parser: '@typescript-eslint/parser', // add the TypeScript parser
+    plugins: [
+        'svelte3',
+        '@typescript-eslint' // add the TypeScript plugin
+    ],
+    overrides: [ // this stays the same
+      {
+        files: ['*.svelte'],
+        parserOptions: { // add these parser options
+            tsconfigRootDir: __dirname,
+            project: ['./tsconfig.json'],
+            extraFileExtensions: ['.svelte'],
+        },
+        extends: [ // then, enable whichever type-aware rules you want to use
+            'eslint:recommended',
+            'plugin:@typescript-eslint/recommended',
+            'plugin:@typescript-eslint/recommended-requiring-type-checking'
+        ],
+        processor: 'svelte3/svelte3'
+      }
+    ],
+    rules: {
+      // ...
+    },
+    settings: {
+      'svelte3/typescript': () => require('typescript'), // pass the TypeScript package to the Svelte plugin
+    }
+  };
