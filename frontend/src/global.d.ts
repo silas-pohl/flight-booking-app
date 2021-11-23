376861b7e458215f05ba61e99b736c3192109a02
silas-pohl <mail@silaspohl.de>
2021-11-23 12:39:06 +0100

    Added Tinro Router and Login/Register Forms

--- frontend/src/global.d.ts
+++ frontend/src/global.d.ts
@@ -1,1 +1,15 @@
 /// <reference types="svelte" />
+
+declare interface LoginData {
+    email: string,
+    password: string
+}
+
+declare interface RegisterData {
+    firstName: string,
+    lastName: string,
+    email: string,
+    password: string
+}
+
+declare type AuthError = "" | "incorrectCred" | "emailTaken" 
