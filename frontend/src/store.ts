74816a8edff2a05f220c733bd8f2b40940c34b15
silas-pohl <mail@silaspohl.de>
2021-12-12 22:45:59 +0100

    Changed frontend to production

--- frontend/src/store.ts
+++ frontend/src/store.ts
@@ -1,5 +1,5 @@
 import { readable, Readable, writable, Writable } from 'svelte/store';
 
-export const API_URL: Readable<string> = readable('http://localhost:80');
+export const API_URL: Readable<string> = readable('https://frontend-flight-booking-app.herokuapp.com/');
 
 export const access_token: Writable<string> = writable('');
