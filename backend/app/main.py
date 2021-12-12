57f1e16e3018a01d6170abfa97c297923303d7ce
silas-pohl <mail@silaspohl.de>
2021-12-13 00:35:12 +0100

    Fixed CORS 2

--- backend/app/main.py
+++ backend/app/main.py
@@ -17,10 +17,14 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+      "http://localhost:5000",
+      "https://frontend-flight-booking-app.herokuapp.com"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
+    allow_credentials=True
 )
 
 # Dependency
